7d3ac3b8f7ad8a5ec8a87ddcf0eb694782ebe129
Tobias Blank <ga49qil@mytum.de>
2020-04-22 21:33:05 +0200

    fixed % and n/a

--- CoCoPreProcessorUI.py
+++ CoCoPreProcessorUI.py
@@ -17,11 +17,10 @@ lFalseNumberMatches = []
 # compile footnote patterns as regex object
 regFootnote = [
     re.compile('\s*\d{1,2}\s*'),
-    re.compile('\s*\*{1,9}\s*'),
-    re.compile('\s*\*{1,9}\)\s*'),
+    re.compile('\s*\(?\*{1,9}\)?\s*'),
     re.compile('\s*[a-z]\s*'),
     re.compile('\s*\d{1,2}\)\s*'),
-    re.compile('\s*\(\d{1,2}\)\s*')
+    re.compile('\s*[(\[]\d{1,2}[)\]]\s*')
 ]
 
 # compile number format as regex objects
@@ -36,10 +35,12 @@ regNumbers = [
     # 123 123,12 ; 123,1 ; 123
     re.compile('(^\s*?[-+$]{0,3}\s*?\(?\d{1,3}(\s\d{3})*?(\.\d{1,5})?\)?\s*?[€%]?\s*?$)', re.MULTILINE),
     # 123 123.12 ; 123.1 ; 123
+    re.compile('^\s*?(%|n\/a)\s*?$'),
+
     # other allowed cell content
     re.compile('^[-.,\s]+$', re.MULTILINE),  # empty cells and placeholder -,.
     re.compile('^\s*?(19|20)\d{2}\s*?$', re.MULTILINE),  # year 1900 - 2099
-    re.compile('^\s*?[0123]?\d?[./-]?[0123]?\d[./-](19|20)?\d{2}\s*?$', re.MULTILINE),
+    re.compile('^\s*?\(?[0123]?\d?[./-]?[0123]?\d[./-](19|20)?\d{2}\)?\s*?$', re.MULTILINE),
     # dates 12.02.1991; 12.31.91: 12.31.2091
     re.compile('^.*[A-Za-z]{2,}.*$', re.DOTALL),  # text
     re.compile('^\s*?(in)?\s*?(T|Tsd|Mio|Mrd)?\.?\s?[€$]\s*?$', re.MULTILINE)  # T€, Mio. €, Mrd. €, in €
